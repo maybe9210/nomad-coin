676088ecf12764dce140fc28c18ba80e74953f54
mason <maybe9210@gmail.com>
2021-08-08 07:28:45 +0900

    exercise array, slice on go lang

--- main.go
+++ main.go
@@ -2,44 +2,12 @@ package main
 
 import "fmt"
 
-const last_name string = "Sim"
-
-func plus(a int, b int, name string) (int, string) {
-	return a + b, name
-}
-
-func plusInRange(a ...int) int {
-	total := 0
-	for _, item := range a {
-		total += item
-	}
-	return total
-}
-
-func printName() {
-	name := "Mason !!!!!!! Is my name"
-
-	for index, item := range name {
-		fmt.Println(index, string(item))
-	}
-}
-
 func main() {
-	// fmt.Println("Welcome to Coin world")
-	// result, ret_name := plus(2, 2, last_name)
-	// fmt.Println(result, ret_name)
-
-	// result_number := plusInRange(1, 3, 4, 2, 5, 23, 3, 4, 5, 54)
-	// fmt.Println(result_number)
-
-	// printName()
-
-	x := 4040404040404041111
-	// fmt.Printf("%b", x)
-	// fmt.Printf("%o", x)
-	// fmt.Printf("%x", x)
-	// fmt.Printf("%U", x)
-
-	xAsBinary := fmt.Sprintf("%b\n", x)
-	fmt.Println(xAsBinary)
+	foods := []string{"potato", "pizza", "pasta"}
+	fmt.Printf("%v\n", foods)
+	// for _, food := range foods {
+	// 	fmt.Println(food)
+	// }
+	foods = append(foods, "tomato")
+	fmt.Printf("%v\n", foods)
 }
