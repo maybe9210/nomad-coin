21f2d68817c91d11e8834948da255e89689c14dc
JeongMin Sim <maybe9210@gmail.com>
2021-08-12 00:59:42 +0000

    exercise struct on go lang

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age int
+}
+
+func (p person) sayHello() {
+	fmt.Printf("Hello! my name is %s and I'm %d\n", p.name, p.age)
+}
+
 func main() {
-	a:=2
-	b:=&a
-	a=5
-	fmt.Println(*b)
+	hulo := person{age: 12, name: "hulo"}
+	hulo2 := person{"hulo2", 13}
+	hulo.sayHello()
+	hulo2.sayHello()
 }
