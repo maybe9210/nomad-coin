9c57110465325ae8c405740bdf9347d5ec853195
JeongMin Sim <maybe9210@gmail.com>
2021-08-12 00:50:24 +0000

    practice pointer on go lang

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	foods := []string{"potato", "pizza", "pasta"}
-	fmt.Printf("%v\n", foods)
-	// for _, food := range foods {
-	// 	fmt.Println(food)
-	// }
-	foods = append(foods, "tomato")
-	fmt.Printf("%v\n", foods)
+	a:=2
+	b:=&a
+	a=5
+	fmt.Println(*b)
 }
