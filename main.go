408824f70d2cf009093ecaa16f8488ba07790755
mason <maybe9210@gmail.com>
2021-08-21 07:10:29 +0900

    Rendering Templates

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
+
+	"github.com/maybe9210/nomad-coin/blockchain"
 )
 
 const port string = ":4000"
 
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
 func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello from home!!")
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	data := homeData{"Home", blockchain.GetBlockChain().GetAllBlocks()}
+	tmpl.Execute(rw, data)
 }
 
 func main() {
