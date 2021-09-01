3a242dbc326b8089327c92e912cfdec1ad2f42d9
mason <maybe9210@gmail.com>
2021-09-02 06:14:56 +0900

    Marshal and Field Tags

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-	"github.com/maybe9210/nomad-coin/explorer"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+
+func documentaion(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentaion",
+		},
+		{
+			URL:         "/blocks",
+			Method:      "POST",
+			Description: "Add a Block",
+			Payload:     "data:string",
+		},
+	}
+	rw.Header().Add("Content-type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+}
+
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentaion)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
