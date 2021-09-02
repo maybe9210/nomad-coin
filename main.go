88e321bba733322c1f023a82f2e3da8e23d9e3f4
mason <maybe9210@gmail.com>
2021-09-03 06:34:30 +0900

    JSON Decode

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/maybe9210/nomad-coin/blockchain"
+	"github.com/maybe9210/nomad-coin/utils"
 )
 
 const port string = ":4000"
@@ -23,6 +26,10 @@ type URLDescription struct {
 	Payload     string `json:"payload,omitempty"`
 }
 
+type AddBlockBody struct {
+	Message string
+}
+
 func documentaion(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
@@ -32,17 +39,42 @@ func documentaion(rw http.ResponseWriter, r *http.Request) {
 		},
 		{
 			URL:         URL("/blocks"),
+			Method:      "GET",
+			Description: "See All Blocks",
+		},
+		{
+			URL:         URL("/blocks"),
 			Method:      "POST",
 			Description: "Add a Block",
 			Payload:     "data:string",
+		},
+		{
+			URL:         URL("/blocks/{id]"),
+			Method:      "GET",
+			Description: "See A Block",
 		},
 	}
 	rw.Header().Add("Content-type", "application/json")
 	json.NewEncoder(rw).Encode(data)
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockChain().GetAllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockChain().AddBlock(addBlockBody.Message)
+		fmt.Printf("posted body.message: %s\n", addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", documentaion)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
