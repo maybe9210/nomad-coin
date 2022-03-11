571866e9adc12f3276c7d0a6894952d4c4ff9425
mason <maybe9210@gmail.com>
2022-03-12 06:42:05 +0900

    Restoring

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,5 +1,14 @@
 package wallet
 
+import (
+	"crypto/x509"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+
+	"github.com/maybe9210/nomad-coin/utils"
+)
+
 const (
 	privateKey    string = "3077020101042049319f52e92f6ba86a384a882f1e9cb3837518cab7441ac5542efcd6b0962bcba00a06082a8648ce3d030107a144034200045522f84857f582bd3ac4ef609487ac0a692baf56de5d70ab7b028d1a709b2dabb3079c95ca23d675a682bfac76dc04ed7ce792dc9950d2f2d243638a24d404c6"
 	signature     string = "7e193ff6bbe34ba6249dc2fe2c320fcd8ea1e1522b08c9dec43198a49547bb0728d903021bb70b83462612328de63ae135f8f2c9419868a5353ce9d5f0cbe532"
@@ -7,31 +16,19 @@ const (
 )
 
 func Start() {
-	// 위 privateKey, signature, hashedMessage 를  만드는 과정
-	// 실제 blockchain, wallet에선 사용되질 않을 코드지만
-	// 이런 흐름으로 private key, signature, hashedMessage가 만들어지고 있다는 것을 알면 좋을듯
+	privBytes, err := hex.DecodeString(privateKey)
+	utils.HandleErr(err)
 
-	// privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	private, err := x509.ParseECPrivateKey(privBytes)
+	fmt.Printf("%d\n\n", private)
+	utils.HandleErr(err)
 
-	// keyAsBytes, err := x509.MarshalECPrivateKey(privateKey)
+	sigBytes, err := hex.DecodeString(signature)
+	utils.HandleErr(err)
+	rBytes := sigBytes[:len(sigBytes)/2]
+	sBytes := sigBytes[len(sigBytes)/2:]
 
-	// fmt.Printf("%x\n\n", keyAsBytes)
-
-	// hashAsBytes, err := hex.DecodeString(hashedMessage)
-
-	// utils.HandleErr(err)
-
-	// r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashAsBytes)
-
-	// signature := append(r.Bytes(), s.Bytes()...)
-
-	// fmt.Printf("signature => %x\n\n", signature)
-
-	// utils.HandleErr(err)
-
-	// fmt.Printf("R:%d\nS:%d\n", r, s)
-
-	// ok := ecdsa.Verify(&privateKey.PublicKey, hashAsBytes, r, s)
-
-	// fmt.Println(ok)
+	var bigR, bigS = big.Int{}, big.Int{}
+	bigR.SetBytes(rBytes)
+	bigS.SetBytes(sBytes)
 }
